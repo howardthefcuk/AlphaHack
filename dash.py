9175bdb7513c6b147d5ab18f961905b4eb541afc
howardthefcuk <howardlecanard@yandex.ru>
2017-08-06 04:22:01 +0300

    Adding dash server

---
+++ dash.py
@@ -0,0 +1,92 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import json
+
+app = dash.Dash()
+
+with open("contrCode.json") as f:
+    country_codes = json.load(f)
+
+data = {
+    'RUS': {
+        "ZHRACHA": [10, 15, 20],
+        "HATA": [100, 110, 400]
+    }
+}
+
+app.layout = html.Div([
+    html.Div([
+        dcc.Location(id='url', refresh=False),
+        html.H1(id='country'),
+        html.H3(id='total'),
+        html.Div([
+            dcc.RadioItems(
+                id='graph-type',
+                options=[{'label': i, 'value': k} for k, i in 
+                    enumerate(['Minimum', 'Average', 'Maximum'])],
+                value=0,
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='days-slider',
+        min=1,
+        max=31,
+        value=1,
+        step=1,
+        marks={str(year): str(year) for year in range(1, 32)}
+    )
+])
+
+@app.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('url', 'pathname'),
+     dash.dependencies.Input('graph-type', 'value'),
+     dash.dependencies.Input('days-slider', 'value')])
+def update_graph(country, graph_type, days_slider):
+    global data
+    country_data = data[country.strip("/")]
+    groups = list(country_data.keys())
+    values = [country_data[group][graph_type] * days_slider for group in groups]
+
+    return {
+        'data': [go.Pie(
+            labels=groups,
+            values=values,
+            textinfo='value',
+            hoverinfo='label+percent'
+        )],
+        'layout': go.Layout(
+            
+        )
+    }
+
+
+@app.callback(
+    dash.dependencies.Output('country', 'children'),
+    [dash.dependencies.Input('url', 'pathname')])
+def update_country(country):
+    return country_codes[country.strip("/")]
+
+@app.callback(
+    dash.dependencies.Output('total', 'children'),
+    [dash.dependencies.Input('url', 'pathname'),
+     dash.dependencies.Input('graph-type', 'value'),
+     dash.dependencies.Input('days-slider', 'value')])
+def update_total(country, graph_type, days_slider):
+    global data
+    country_data = data[country.strip("/")]
+    groups = list(country_data.keys())
+    values = [country_data[group][graph_type] * days_slider for group in groups]
+    return "Total: {} RUB".format(sum(values))
+
+if __name__ == '__main__':
+    app.run_server()
